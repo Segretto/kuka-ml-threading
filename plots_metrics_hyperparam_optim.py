06b8579d780c8dfc9a33344e7bb88185c8defaf4
Gustavo Jose Giardini Lahr <gustavo.giardini@iit.it>
2022-08-16 16:27:32 +0200

    added plots

---
+++ plots_metrics_hyperparam_optim.py
@@ -0,0 +1,83 @@
+import json
+import os
+from typing import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+
+models = ['mlp', 'cnn', 'lstm', 'transf']
+models_ticks = ['MLP', 'CNN', 'LSTM', 'Transformer']
+datasets = ['original', 'nivelado', 'quadruplicado']
+# datasets_ticks = ['Original', 'Balanced', 'Augmented']
+datasets_ticks = ['Ori.', 'Bal.', 'Aug.']
+PARAMETERS=['fx|fy|fz|mx|my|mz']#, 'rotx|fx|fy|fz|mx|my|mz']
+N_EPOCHS=100
+
+plt.style.use('plot_style.txt')
+
+fig, ax = plt.subplots(1, 1)
+my_xticks = []
+ctr = 0
+
+for parameters in PARAMETERS:
+    for idx_model, model_name in enumerate(models):
+        for idx_dataset, dataset_name in enumerate(datasets):
+
+            experiment_name = model_name + '_' + dataset_name
+            experiment_name += '_' + str(N_EPOCHS) + '_epochs'
+            if 'rot' in parameters:
+                experiment_name += '_with_rot'
+            file_name = experiment_name + '/best_' + model_name + '_' + dataset_name + '.json'
+            # dir_abs = os.getcwd()
+            dir_abs = r'/home/glahr/git/kuka-ml-threading/output/models_meta_data/'
+            with open(dir_abs + file_name, 'r') as f:
+                hyperparameters = json.load(f)
+
+            mean = np.mean(hyperparameters['user_attrs_classification_reports'])
+            std = np.std(hyperparameters['user_attrs_classification_reports'])
+            # ax[idx_model].bar(idx_dataset, mean, yerr=std, width=0.6)
+            if idx_dataset == 0:
+                color = '#bc80bd'
+            if idx_dataset == 1:
+                color = '#fb8072'
+            if idx_dataset == 2:
+                color = '#b3de69'
+            ax.bar(ctr+idx_dataset, mean, yerr=std, width=0.6, color=color)
+            my_xticks.append(ctr+idx_dataset)
+        ctr += 4
+
+# for idx_model in range(len(models)):
+#     ax[idx_model].set_xticks([i for i in range(len(datasets))])    
+#     ax[idx_model].set_xticklabels(datasets_ticks, fontsize=12)
+#     ax[idx_model].set_axisbelow(True)
+#     ax[idx_model].set_title(models_ticks[idx_model])
+#     ax[idx_model].set_ylim([0, 1.1])
+#     ax[idx_model].spines['right'].set_visible(False)
+#     ax[idx_model].spines['top'].set_visible(False)
+#     if idx_model != 0:
+#         ax[idx_model].spines['left'].set_visible(False)
+#         ax[idx_model].set_yticklabels([])
+#     ax[idx_model].grid(visible=True, axis='y')
+
+
+my_xtickslabels = len(models)*datasets_ticks
+ax.set_xticks(my_xticks)
+ax.set_xticklabels(my_xtickslabels, fontsize=9)
+ax.set_axisbelow(True)
+ax.set_yticks([0, 0.25, 0.5, .6, 0.7, .8, .9, 1.0])
+ax.set_yticklabels([0, 0.25, 0.5, .6, 0.7, .8, .9, 1.0], fontsize=9)
+ytext = 1.15
+ax.text(.5, ytext, models_ticks[0])
+ax.text(4.5, ytext, models_ticks[1])
+ax.text(8.5, ytext, models_ticks[2])
+ax.text(11.8, ytext, models_ticks[3])
+ax.set_ylim([0.5, 1.1])
+ax.set_xlim([-1, my_xticks[-1]+1])
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.grid(visible=True, axis='y')
+title = 'With rotation' if 'rotx' in PARAMETERS[0] else 'Without rotation'
+ax.set_title(title)
+ax.set_ylabel('Precision [-]', fontsize=14)
+fig.autofmt_xdate(rotation=30)
+plt.tight_layout()
+plt.show()
---
+++ plots_timesteps_trained.py
@@ -0,0 +1,74 @@
+import json
+import os
+from typing import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+from src.ml_dataset_manipulation import DatasetManip
+
+models = ['mlp', 'cnn', 'lstm', 'transf']
+models_ticks = ['MLP', 'CNN', 'LSTM', 'Transformer']
+datasets = ['original', 'nivelado', 'quadruplicado']
+# datasets_ticks = ['Original', 'Balanced', 'Augmented']
+datasets_ticks = ['Ori.', 'Bal.', 'Aug.']
+states = ['Mounted', 'Jammed', 'Not mounted']
+states_colors = ['g', 'r', 'b']
+PARAMETERS=['fx|fy|fz|mx|my|mz']#, 'rotx|fx|fy|fz|mx|my|mz']
+N_EPOCHS=100
+
+plt.style.use(r'/home/glahr/git/kuka-ml-threading/plot_style.txt')
+
+idx_timesteps = np.random.randint(96)
+print(idx_timesteps)
+
+# training
+fig, ax = plt.subplots(1, 1)
+
+for parameters in PARAMETERS:
+    for idx_model, model_name in enumerate(models):
+        for idx_dataset, dataset_name in enumerate(datasets):
+            
+            experiment_name = model_name + '_' + dataset_name
+            experiment_name += '_' + str(N_EPOCHS) + '_epochs'
+            experiment_name += '_with_rot' if 'rot' in parameters else ''
+            file_name = experiment_name + '/' + model_name + '_' + dataset_name + '_' + str(N_EPOCHS) + '_epochs'
+            file_name += '_with_rot' if 'rot' in parameters else ''
+            file_name += '_trained.json' if True else ''
+            # dir_abs = os.getcwd()
+            dir_abs = r'/home/glahr/git/kuka-ml-threading/output/models_meta_data/'
+            with open(dir_abs + file_name, 'r') as f:
+                data = json.load(f)
+            
+            dataset = DatasetManip( dataset_name=dataset_name,
+                                    model_name=model_name,
+                                    parameters=parameters)
+
+            # name = model_name + '_' + dataset_name
+            # file_name = name + '/metrics_full_train_' + name + '.json'
+            # dir_abs = r'/home/glahr/git/kuka-ml-threading/output/models_trained/'
+            # with open(dir_abs + file_name, 'r') as f:
+            #     data = json.load(f)
+
+            prec = np.mean(data['report']['mounted']['precision'])
+            # for yt in data['y_timesteps']:
+            y_pred_timesteps = data['y_timesteps'][idx_timesteps]
+            last_i = 0
+            for i, yi in enumerate(y_pred_timesteps):
+                if y_pred_timesteps[i] == y_pred_timesteps[last_i] and i != len(y_pred_timesteps)-1:
+                    continue
+                else:
+                    ax.axvspan(last_i*.12, i*.12, facecolor=states_colors[y_pred_timesteps[last_i]], alpha=0.2, label='_nolegend_')
+                    last_i = i
+                
+            d = dataset.scaler.inverse_transform(dataset.X_test[idx_timesteps]).T
+            
+            ax.plot(np.arange(290)*.12, d[2], 'b')
+
+            ax.plot(np.arange(290)*.12, data['y_test'][idx_timesteps]*np.ones_like(y_pred_timesteps), 'r--')
+            ax.plot(np.arange(290)*.12, y_pred_timesteps, 'k')
+            # ax.set_ylim([-0.1, 2.1])
+            ax.set_title(model_name + ' in dataset ' + dataset_name)
+            # ax.set_xlim([-1, 33])
+            # ax.set_yticks([0, 1, 2])
+            # ax.set_yticklabels(states)
+            ax.legend(['Actual', 'Prediction'])
+            plt.show()
